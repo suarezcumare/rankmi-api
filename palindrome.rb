75d4fce9b2d9391561e78a6df68e6c5a30ce2a20
suarezcumare <suarezcumare@gmail.com>
2017-10-19 20:45:11 -0300

    Update palindrome.rb
    explicando lo que se hizo

--- palindrome.rb
+++ palindrome.rb
@@ -1,5 +1,6 @@
 #palindromo
-
+#solucion, sin mucho que decir
+#Solo en dada la palabra si es igual al contrario y al reverse, retorna palindromo
 def palindromo(text)
   if text.downcase == text.downcase.reverse
     "palindrome"
@@ -13,7 +14,8 @@ end
 
 #solucion
 #En esta solucion com te comente ruby tiene para hacer el recorrido sin problemas, lo que hice fue iterar hasta el numero
-#que necesitaba y al final solo obtener el valor necesitado
+#que necesitaba y al final solo obtener el valor necesitado, teniendo la constante de hash guarda al ejecutar la app
+#se ejecutara una vez, lo demas ya seria solo buscar la solucion rapida y sencilla
 h = {}
 ('a'..'zzz').each_with_index{|w, i| h[i+1] = w }
 
@@ -24,6 +26,11 @@ end
 
 
 #.- Crear un algoritmo al cual se le deba ingresar un array de n números enteros, más un numero de resultado, ejemplo: Input: numbers={2, 7, 11, 15}, target=9 Este algoritmo debe ser capaz de encontrar dos números dentro del array que sumados sean igual al numero target,obviamente el algoritmo no puede ser un for que vaya sumando todos los números de uno en uno, debe tener mayor inteligencia.El resultado debe indicar la posición del array de los números que sumados dan el resultado esperado, ejemplo: Output: index1=1, index2=2
+
+
+#solucion
+#en ruby existe combination par aun numero de falores, entonces recorro todas las posibles opciones y 
+#alli lo que hago es iterar por cada una de ella y obtener lo que necesito al final
 
  #numbers( [2, 7, 11, 15], 9)
  def numbers(array = [], sum = 0)
